01428351f0b97985f4715135c955821665bd75f3
brian mureithi <brianmurithi65@gmail.com>
2020-12-16 16:42:47 +0300

    My first Push

---
+++ src/components/Heading.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = styled.header`
+max-width:70rem;
+margin:2 rem auto;
+
+
+`;
+const H1 = styled.h1`
+`;
+
+export const Heading = () => {
+    return (
+        <div>
+          <h1>Image Loader </h1>  
+          <p></p>
+        </div>
+    )
+}
---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader = () => {
+    return (
+        <div>
+           <h1>Spinner</h1> 
+        </div>
+    )
+}
